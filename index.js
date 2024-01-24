ee6f4544160067d238e9d7b4d3d941da9cd41bf5
Kartik Saxena <karrtiksaxena@gmail.com>
2024-01-24 13:03:16 +0530

    userjs

--- index.js
+++ index.js
@@ -7,7 +7,11 @@ console.log("hello");
 
 app.get("/api",(req,res)=>{
    const result= console.log("hello mere bete");
-   res.send(result);
+
+ const html =  <html>
+      <body>{result}</body>
+   </html>
+      res.send(html);
 })
 
 app.listen(PORT,()=>console.log(`the server started at port number ${PORT}`));
