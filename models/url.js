c134ddc128e0505afc261472ad040434db960687
Kartik Saxena <karrtiksaxena@gmail.com>
2024-01-26 00:00:04 +0530

    new

--- models/url.js
+++ models/url.js
@@ -1,6 +1,22 @@
 const mongoose = require ("mongoose");
 
 const userSchema = new mongoose.Schema({
+    shortId:{
+        type:String,
+        required:true,
+        unique:true,
+    },
+    redirectUrl:{
+        type:String,
+        required:true,
 
-    
-})
+    },
+    totalClicks:[{timestamp:{type:Number}}],
+},
+    { timestamps: true}
+   
+    );
+
+const URL = mongoose.model("Url", userSchema);
+
+module.exports=URL;
